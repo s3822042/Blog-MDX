32b494325ba0d48a7ef47d56631a1baea719419c
s3822042 <s3822042@rmit.edu.vn>
2023-01-03 22:48:51 +0700

    update code

--- pages/categories.tsx
+++ pages/categories.tsx
@@ -1,32 +1,47 @@
 import React from "react";
-import { select } from "utils/select";
-import { allArticles } from "contentlayer/generated";
+import {select} from "utils/select";
+import {allArticles} from "contentlayer/generated";
 import Category from "components/Category";
-import { NextSeo } from "next-seo";
-import { siteMetadata } from "utils/constant";
+import {NextSeo} from "next-seo";
+import {siteMetadata} from "utils/constant";
 
-export default function Categories({ articles }: any) {
-  const categories = [{ title: "Software Engineer", count: 2 }];
+export default function Categories({articles}: any) {
+    const counts = {} as any;
 
-  return (
-    <>
-      <NextSeo
-        title={`Categories - ${siteMetadata.author}`}
-        description="Things I blog about"
-      />
-      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
-        <div className="pt-6 pb-8 space-x-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
-            Categories
-          </h1>
-        </div>
-        <div className="flex flex-wrap max-w-lg">
-          {Object.keys(categories).length === 0 && "No categories found."}
-          {Object.values(categories).map((t: any) => {
-            return (
-              <div key={t} className="mt-2 mb-2 mr-5">
-                <Category text={t.title} />
-                <span className="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300">
+    articles.forEach((category: any) => {
+        category.categories.forEach((subCategory: any) => {
+            const title = subCategory.title;
+            if (counts[title]) {
+                counts[title]++;
+            } else {
+                counts[title] = 1;
+            }
+        });
+    });
+
+    const categories = Object.entries(counts).map(([title, count]) => ({title, count}));
+
+    return (
+        <>
+            <NextSeo
+                title={`Categories - ${siteMetadata.author}`}
+                description="Things I blog about"
+            />
+            <div
+                className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+                <div className="pt-6 pb-8 space-x-2 md:space-y-5">
+                    <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+                        Categories
+                    </h1>
+                </div>
+                <div className="flex flex-wrap max-w-lg">
+                    {Object.keys(categories).length === 0 && "No categories found."}
+                    {Object.values(categories).map((t: any) => {
+                        return (
+                            <div key={t} className="mt-2 mb-2 mr-5">
+                                <Category text={t.title}/>
+                                <span
+                                    className="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300">
                   {` (${t.count})`}
                 </span>
               </div>
--- pages/tags.tsx
+++ pages/tags.tsx
@@ -1,27 +1,37 @@
 import React from "react";
-import { select } from "utils/select";
-import { allArticles } from "contentlayer/generated";
+import {select} from "utils/select";
+import {allArticles} from "contentlayer/generated";
 import Tag from "components/Tag";
-import { NextSeo } from "next-seo";
-import { siteMetadata } from "utils/constant";
+import {NextSeo} from "next-seo";
+import {siteMetadata} from "utils/constant";
 
 export default function Tags({ articles }: any) {
 
-  const tags = [
-    { title: "react", count: 1 },
-    { title: "redux", count: 2 },
-    { title: "typescript", count: 1 },
-  ];
+    const counts = {} as any;
 
-  return (
-    <>
-      <NextSeo
-        title={`Tags - ${siteMetadata.author}`}
-        description="Things I blog about"
-      />
-      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
-        <div className="pt-6 pb-8 space-x-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+    articles.forEach((category: any) => {
+        category.tags.forEach((subCategory: any) => {
+            const title = subCategory.title;
+            if (counts[title]) {
+                counts[title]++;
+            } else {
+                counts[title] = 1;
+            }
+        });
+    });
+
+    const tags = Object.entries(counts).map(([title, count]) => ({title, count}));
+
+    return (
+        <>
+            <NextSeo
+                title={`Tags - ${siteMetadata.author}`}
+                description="Things I blog about"
+            />
+            <div
+                className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+                <div className="pt-6 pb-8 space-x-2 md:space-y-5">
+                    <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
             Tags
           </h1>
         </div>
