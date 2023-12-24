f340f2b5552cff6ed4caf83d06444071d16090d1
s3822042 <s3822042@rmit.edu.vn>
2023-12-25 00:40:30 +0700

    add article

--- .contentlayer/generated/Article/_index.mjs
+++ .contentlayer/generated/Article/_index.mjs
@@ -4,5 +4,6 @@ import articles__authenticationWithNextAndPrismaMdx from './articles__authentica
 import articles__dataModellingUsingScikitLearnMdx from './articles__data-modelling-using-scikit-learn.mdx.json' assert { type: 'json' }
 import articles__frontEndFrameworkChoice_2023Mdx from './articles__front-end-framework-choice-2023.mdx.json' assert { type: 'json' }
 import articles__seriesAlgorithmValidAnagramMdx from './articles__series-algorithm-valid-anagram.mdx.json' assert { type: 'json' }
+import articles__seriesAlgorithmTwoSumMdx from './articles__series-algorithm-two-sum.mdx.json' assert { type: 'json' }
 
-export const allArticles = [articles__authenticationWithNextAndPrismaMdx, articles__dataModellingUsingScikitLearnMdx, articles__frontEndFrameworkChoice_2023Mdx, articles__seriesAlgorithmValidAnagramMdx]
+export const allArticles = [articles__authenticationWithNextAndPrismaMdx, articles__dataModellingUsingScikitLearnMdx, articles__frontEndFrameworkChoice_2023Mdx, articles__seriesAlgorithmValidAnagramMdx, articles__seriesAlgorithmTwoSumMdx]
