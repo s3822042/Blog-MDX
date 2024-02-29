3dd46d3bcaab65789d713d57151be45fbbaf85c2
s3822042 <s3822042@rmit.edu.vn>
2024-03-01 02:07:14 +0700

    fix: hydration error, client side UI sorting not match with server side hydration

--- src/www/components/modules/comments/comment-list-section.tsx
+++ src/www/components/modules/comments/comment-list-section.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import { formatDate, sortByDescending } from "@/lib/utils"
+import { useMounted } from "@/hooks/use-mounted"
 import { Heading } from "@/components/elements/heading"
 import { trpc } from "@/app/_trpc/client"
 
 export function CommentListSection() {
+  const mounted = useMounted()
   const { data } = trpc.getComments.useQuery({ limit: 10, page: 1 })
 
   if (!data) {
@@ -20,14 +22,16 @@ export function CommentListSection() {
           <Heading as="h2" size="3xl" className="mt-12">
             What people are saying
           </Heading>
-          {sortedComments.map((comment, index) => (
-            <div className="my-4 border p-6" key={index}>
-              <header className="text-sm">
-                Posted by {comment.nickname} on {formatDate(comment.createdAt)}
-              </header>
-              <p className="mt-4">{comment.content}</p>
-            </div>
-          ))}
+          {mounted &&
+            sortedComments.map((comment, index) => (
+              <div className="my-4 border p-6" key={index}>
+                <header className="text-sm">
+                  Posted by {comment.nickname} on{" "}
+                  {formatDate(comment.createdAt)}
+                </header>
+                <p className="mt-4">{comment.content}</p>
+              </div>
+            ))}
         </>
       ) : (
         <p className="text-center">No Comments Found</p>
