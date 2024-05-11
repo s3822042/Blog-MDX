3410e24fede2f4b6b1a97ce1f3c3d9fc9f612760
s3822042 <s3822042@rmit.edu.vn>
2024-05-11 12:26:06 +0700

    fix: scripts rss

--- src/www/scripts/rss.mjs
+++ src/www/scripts/rss.mjs
@@ -4,15 +4,15 @@ import { allArticles } from "../.contentlayer/generated/index.mjs";
 
 const feed = new RSS({
   title: "Personal Blog",
-  feed_url: "https://ciara-blog.netlify.app/rss.xml",
-  site_url: "https://ciara-blog.netlify.app/",
+  feed_url: "https://blog.luanvothanh.com/rss.xml",
+  site_url: "https://blog.luanvothanh.com/",
 });
 
 allArticles
   .map((article) => ({
     title: article.title,
     description: article.description,
-    url: `https://ciara-blog.netlify.app/articles/${article.slug}`,
+    url: `https://blog.luanvothanh.com${article.slug}`,
     date: article.publishedAt,
   }))
   .forEach((item) => {
